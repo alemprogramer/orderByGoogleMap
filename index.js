5324755b32fd7a36ea56b7903afbde8cfb3f0f37
alemprogramer <alemhhaisb@gmail.com>
2020-11-14 12:04:41 +0600

    mobile browser issu fixed

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ function initMap() {
 
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 16,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
         center: uluru
     });
 
